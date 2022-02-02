82d0d7c22699244bbff9dd9d15ce17ba8af65fe3
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2022-02-02 15:38:21 -0500

    complete nft

--- script.py
+++ script.py
@@ -34,7 +34,12 @@ class Eyes:
     }
 
 class Hair:
-    hair = ['Up Hair', 'Down Hair', 'Mohawk', 'Red Mohawk', 'Orange Hair', 'Bubble Hair', 'Thin Hair', 'Bald', 'Blonde Hair', 'Caret Hair', 'Pony Tails']
+    hair = ['Up Hair', 'Down Hair', 'Mohawk', 'Red Mohawk', 'Orange Hair', 'Bubble Hair', 'Emo Hair',
+    'Thin Hair',
+    'Bald',
+    'Blonde Hair',
+    'Caret Hair',
+    'Pony Tails']
     hair_weights = [10 , 10 , 10 , 10 ,10, 10, 10 ,10 ,10, 7 , 1 , 2]
     hair_files = {
     "Up Hair": "hair1",
@@ -49,7 +54,8 @@ class Hair:
     "Blonde Hair": "hair10",
     "Caret Hair": "hair11",
     "Pony Tails": "hair12"
-    }
+}
+
 
 class Mouth:
     mouth = ['Black Lipstick', 'Red Lipstick', 'Big Smile', 'Smile', 'Teeth Smile', 'Purple Lipstick']
@@ -71,20 +77,25 @@ class Nose:
         "Nose Ring": "n2"   
     }
 
-
+face = Face()
+ears = Ears()
+eyes = Eyes()
+hair = Hair()
+mouth = Mouth()
+nose = Nose()
 class Create_NFT:
     user_num = int(input("How many NFTs would you like to create?: "))
     all_images = []
-
+        
+    def __init__(self):
+        self.create_image()
+        self.unique_combinations()
+        self.unique_correspondance()
+        self.get_trait_counts()
+        self.generate_nfts()
 
     def create_image(self):
         new_image = {}
-        face = Face()
-        ears = Ears()
-        eyes = Eyes()
-        hair = Hair()
-        mouth = Mouth()
-        nose = Nose()
         new_image['Face'] = random.choices(face.face, face.face_weights)[0]
         new_image['Ears'] = random.choices(ears.ears, ears.ears_weights)[0]
         new_image['Eyes'] = random.choices(eyes.eyes, eyes.eyes_weight)[0]
@@ -98,12 +109,88 @@ class Create_NFT:
             return new_image 
 
     def unique_combinations(self):
-        for i in range(user_num):
+        for i in range(self.user_num):
             new_trait_image = self.create_image()
             self.all_images.append(new_trait_image)
-             
+
+    def unique_checker(self, all_images):
+        seen = list()
+        return not any(i in seen or seen.append(i) for i in self.all_images)
+
+    def unique_correspondance(self):
+        print("Are all images unique?: {}".format(self.unique_checker(self.all_images)))
+        i = 0 
+        for item in self.all_images:
+            item['tokenId'] = i
+            i += 1
+        print(self.all_images)
+    
+    def get_trait_counts(self):
+        face_count = {}
+        for item in face.face:
+            face_count[item] = 0
+            
+        ears_count = {}
+        for item in ears.ears:
+            ears_count[item] = 0
+
+        eyes_count = {}
+        for item in eyes.eyes:
+            eyes_count[item] = 0
+            
+        hair_count = {}
+        for item in hair.hair:
+            hair_count[item] = 0
+            
+        mouth_count = {}
+        for item in mouth.mouth:
+            mouth_count[item] = 0
+            
+        nose_count = {}
+        for item in nose.nose:
+            nose_count[item] = 0
+
+        for image in self.all_images:
+            face_count[image["Face"]] += 1
+            ears_count[image["Ears"]] += 1
+            eyes_count[image["Eyes"]] += 1
+            hair_count[image["Hair"]] += 1
+            mouth_count[image["Mouth"]] += 1
+            nose_count[image["Nose"]] += 1
+            
+        print(face_count)
+        print(ears_count)
+        print(eyes_count)
+        print(hair_count)
+        print(mouth_count)
+        print(nose_count)
+
+    def generate_nfts(self):
+        os.mkdir(f'./images')
+
+        for item in self.all_images:
+
+            im1 = Image.open(f'./face_parts/face/{face.face_files[item["Face"]]}.png').convert('RGBA')
+            im2 = Image.open(f'./face_parts/eyes/{eyes.eyes_files[item["Eyes"]]}.png').convert('RGBA')
+            im3 = Image.open(f'./face_parts/ears/{ears.ears_files[item["Ears"]]}.png').convert('RGBA')
+            im4 = Image.open(f'./face_parts/hair/{hair.hair_files[item["Hair"]]}.png').convert('RGBA')
+            im5 = Image.open(f'./face_parts/mouth/{mouth.mouth_files[item["Mouth"]]}.png').convert('RGBA')
+            im6 = Image.open(f'./face_parts/nose/{nose.nose_files[item["Nose"]]}.png').convert('RGBA')
+
+            #Create each composite
+            com1 = Image.alpha_composite(im1, im2)
+            com2 = Image.alpha_composite(com1, im3)
+            com3 = Image.alpha_composite(com2, im4)
+            com4 = Image.alpha_composite(com3, im5)
+            com5 = Image.alpha_composite(com4, im6)
+
+                            
+
+            #Convert to RGB
+            rgb_im = com5.convert('RGB')
+            file_name = str(item["tokenId"]) + ".png"
+            rgb_im.save("./images/" + file_name)
                
 
 
-
-
+test = Create_NFT()
